21f68fbf277b8f3b9d1334594ec993214ec3682c
Martin <joewwi03@gmail.com>
2023-04-01 22:13:11 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,5 +1,21 @@
 const init = () => {
-  
+    const inputForm = document.querySelector("form")
+    inputForm.addEventListener('submit', (event) => {
+        event.preventDefault()
+        const input = document.getElementById('searchByID')
+        console.log(input.value);
+
+        fetch(`http://localhost:3000/movies/${input.value}`)
+        .then(resp => resp.json())
+        .then((moviesData) => {
+            const title = document.querySelector('section#movieDetails h4');
+            const summary = document.querySelector('section#movieDetails p');
+
+            title.innerText = moviesData.title
+            summary.innerText = moviesData.summary
+            
+        })
+    })
 }
 
 document.addEventListener('DOMContentLoaded', init);
